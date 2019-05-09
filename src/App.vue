68776b3e0d8b638cbb9fa0e65c65f3ca02d9f35a
Piotr Mazur <mazur.piotr93@gmail.com>
2019-05-09 22:57:43 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <nav id="nav">
+      <router-link to="/">Search</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="home">
+    <h1>spacer</h1>
+  </main>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'search',
+  components: {
+  },
+};
+</script>
